63721ad408d69a3600b5730726ea7856a4778322
= <dastan.emilbekov0101@mail.ru>
2021-02-27 15:57:54 +0600

    new js commit

---
+++ js/practice.js
@@ -0,0 +1,78 @@
+// my project
+//  const numberOfFilms = [];
+
+//  numberOfFilms[0] = prompt('Сколько фильмов вы посморели?', '');
+
+//  let whatisFilms = [];
+
+//  whatisFilms[0] = prompt('Один из последних просмотренных фильмов', '');
+//  whatisFilms[1] = prompt('На сколько оцените его?', '');
+//  whatisFilms[2] = prompt('Один из последних просмотренных фильмов', '');
+//  whatisFilms[3] = prompt('На сколько оцените его?', '');
+
+//  const personalMovieDB = {
+//      count: numberOfFilms[0],
+//      movies: {
+//             filmsname: whatisFilms[0],
+//             rating: whatisFilms[1]
+//              },
+//      actors: {},
+//      genres: [],
+//      privat: false
+// };
+
+// console.log(personalMovieDB.count, personalMovieDB.movies.filmsname, personalMovieDB.movies.rating,  whatisFilms[0]);
+/*jshint esversion: 6 */
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+// const personalMovieDB = {
+//       count: numberOfFilms,
+//       movies: {},
+//       actors: {},
+//       genre: [],
+//       privar: false
+// };
+
+// const a = prompt('One of you last seen movies?', ''),
+//       b = prompt('How would you rate it?', ''),
+//       c = prompt('One of you last seen movies?', ''),
+//       d = prompt('How would you rate it?', '');
+
+// personalMovieDB.movies [a] = b;
+// personalMovieDB.movies [c] = d;
+
+// console.log(personalMovieDB.movies);
+
+
+ const numberOfCars = +prompt('How many cars do you hove', '');
+  
+ const personalCarDB = {
+       carcount: numberOfCars,
+       cars: {numberOfCars,},
+       mark: [],
+       cost: {},
+ };
+
+ const a = prompt('The last car that you ride', '');
+       b = prompt('Describe your emotions', '');
+       c = prompt('The last car that you ride', '');
+       d = prompt('Describe your emotions', '');
+
+personalCarDB.mark [a] = b;
+personalCarDB.mark [c] = d;
+
+console.log(personalCarDB);
+
+// if (personalCarDB.carcount < 10) {
+//       console.log('You have seen a few movies');
+// } else if (personalCarDB.carcount > 15){
+//       console.log('you Have seen a lot of cars');
+// } else {
+//       console.log('i didnt ');
+// }
+for (let i = 0; i < 2; i++) {
+      const a = prompt('The last car that you ride', '');
+            b = prompt('Describe your emotions', '');
+      personalCarDB.mark [a] = b;
+}
+Hello
