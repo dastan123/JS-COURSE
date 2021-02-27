c6014443aff2e2f8819b759a140b9a48660b8d07
Dastan <dastan@gmail.com>
2021-02-27 08:04:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,153 @@
+"use strict";
+ /* global alert */
+// var, let, const
+// Strings, Numbers, Boolean, null, undefined, Symbol
+
+// const name = "John";
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+// console.log(typeof z);
+
+
+// const name = "Jogn";
+// const age = 30;
+// //Concatenation
+// console.log('My name is ' + name + ' and i am ' + age);
+// // Template String
+// const hello = `My name is ${name} and i am ${age}`;
+// console.log (hello);
+
+
+// const  s = 'techology, computers, it, code';
+// console.log(s.split(','));
+
+
+// Arrays - variables that hold multiple values
+// const numbers = new Array(1,2,3,4,5);
+
+// const hello = new Array(1,2,3,4,5);
+// const fruits = ['apples', 'oranges', 'pears'];
+
+// fruits[5] ='grapes';
+
+// fruits.push('mangas');
+
+// fruits.unshift('strawberries');//puts in the begining
+
+// fruits.pop();
+
+// console.log(Array.isArray()); //checking array
+
+// console.log(fruits.indexOf('grapes'));
+
+// console.log(fruits);
+
+// const person = {
+//       firstName: 'John',
+//       lastName: 'Doe',
+//       age: 30,
+//       hobbies: ['music', 'movies', 'sports'],
+//       address: {
+//             street: '50 main st',
+//             city: 'Boston',
+//             state: 'Ma'
+//       }
+// };
+
+// console.log(person.firstName, person.lastName, person.hobbies[1]), person.address[1];
+
+// let hell;
+// console.log(hell);
+
+// const person = {
+//       hobbies:['music', 'movies']
+// };
+
+// console.log(person.hobbies[1]);
+
+// let arr = ['hello', 'grapes', 'mandalini', 6, {}, []];
+
+// console.log(arr[1]);
+
+// alert('hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+
+// console.log(answer + 5);
+
+// const answers = [];
+
+//   answers[0] = prompt('Как ваше имя?', '');
+//   answers[1] = prompt('Как ваше фамилия?', '');
+//   answers[2] = prompt('Сколько вам лет?', '');
+
+//  console.log(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hello`, ${user});
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decro = 10;
+
+// incr++; + 1 to number
+// decro--; - 1 to number
+
+// console.log(++incr);
+// console.log(--decro);
+
+// console.log(5%2);
+// console.log(2*4 != '8');
+
+// const isChecked = false,
+//       isClosed = false;
+
+// console.log(isChecked || !isClosed); 
+
+// let incr = 10 == 50;
+
+// ++incr;
+// console.log(incr);
+
+
+
+/* my project
+ const numberOfFilms = [];
+
+ numberOfFilms[0] = prompt('Сколько фильмов вы посморели?', '');
+
+ let whatisFilms = [];
+
+ whatisFilms[0] = prompt('Один из последних просмотренных фильмов', '');
+ whatisFilms[1] = prompt('На сколько оцените его?', '');
+ whatisFilms[2] = prompt('Один из последних просмотренных фильмов', '');
+ whatisFilms[3] = prompt('На сколько оцените его?', '');
+
+ const personalMovieDB = {
+     count: numberOfFilms[0],
+     movies: {
+            filmsname: whatisFilms[0],
+            rating: whatisFilms[1]
+             },
+     actors: {},
+     genres: [],
+     privat: false
+};
+
+console.log(personalMovieDB.count, personalMovieDB.movies.filmsname, personalMovieDB.movies.rating,  whatisFilms[0]);
+
+*/
