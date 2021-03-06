18540ee6cdfe5866349c4f3e32587c52b99b511c
Dastan <dastan@gmail.com>
2021-03-06 11:32:07 +0600

    date

--- js/learn.js
+++ js/learn.js
@@ -1,78 +1,83 @@
+const now = new Date(0);
+// new Date.parse('2021-03-06');
 
-const btn = document.querySelector('.button'),
-      btnDelete = document.querySelector('.delete-button'),
-      box = document.querySelectorAll('.box'),
-      boxBig = document.querySelector('.box-2'),
-      set = document.querySelector('.page-set'),
-      form = document.querySelector('.register-form');
-
-function showNextPage(i = 0) {
-      box[i].classList.remove('hide'); 
-      box[i].classList.add('show'); 
-}
-
-function hideNextPage() {
-      box.forEach(item => {
-      item.classList.remove('show'); 
-      item.classList.add('hide'); 
-      });
-}
-
-hideNextPage();
-showNextPage();
-
-btn.addEventListener('click', e => {
- 
-      if(box[0].classList.contains('show')) {
-            hideNextPage();
-      }
-      if (box[1].classList.contains('hide'))
-      {
-            showNextPage(1);
-      }
-});
-
-btnDelete.addEventListener('click', e => {
-      if(box[1].classList.contains('show')) {
-            hideNextPage();
-      }
-      if(box[0].classList.contains('hide')) {
-            showNextPage();
-      }
-});
-// let num = '1';
-// // num.innerHTML = "<a>kalsdf</a>";
-// num.
-// set.before(num);
+// console.log(now.getday());
 
 
-function hideForm(arr) {
-      arr.classList.add('hide');
-}
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+console.log(now.getDay());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getFullYear());
+console.log(now.getHours());
+console.log(now.getMinutes());
 
-function showForm() {
+console.log(now.getTimezoneOffset());
+console.log(now.getTime());
 
-   
-      if (form.classList.contains('hide')) {
-            form.classList.remove('hide');
-            form.classList.add('show');
-      }
-  
-     
-      console.log('hello');
-}
+// let start = new Date();
 
-function showBigForm() {
-      if (form.classList.contains('show')) {
-            form.classList.remove('show');
-            form.classList.add('hide');
-      }
-}
+// for (let i = 0; i < 100000; i++) {
+//       let some = i ** 3;
+// }
 
-hideForm(form);
+// let end = new Date();
 
-const timeI = setInterval(showBigForm, 1000);
-const timeId = setInterval(showForm, 1000);
+// alert(`it worked ${end - start}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
