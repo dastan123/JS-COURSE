f988db1f6bc92cfe7773dbb89a1f709b7acb53c5
Dastan <dastan@gmail.com>
2021-03-07 16:08:21 +0600

    modified modal window

--- js/learn.js
+++ js/learn.js
@@ -1,79 +1,115 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  // Modal
 
-// Modal
+  const btnModal = document.querySelectorAll(".btn-modal"),
+    btnClose = document.querySelector(".btn-close"),
+    modal = document.querySelector(".modal"),
+    btnWrapper = document.querySelector(".button-container");
 
-const btnModal = document.querySelectorAll('.btn-modal'),
-      btnClose = document.querySelector('.btn-close'),
-      modal = document.querySelector('.modal'),
-      btnWrapper = document.querySelector('.button-container');
+  function hideModal() {
+    // modal.style.display = "none";
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+    document.body.style.cssText = "pointer-events: auto;";
 
-      function hideModal() {
-            // modal.style.display = "none";
-            modal.classList.add('hide');
-            modal.classList.remove('show');
-            document.body.style.overflow = '';
-      }
+  }
 
-      function showModal() {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    document.body.style.cssText = "pointer-events: none;";
+    modal.style.cssText = "pointer-events: auto;";
+  }
 
-      }
+  function showModal(i) {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    document.body.style.cssText = "pointer-events: none;";
+    modal.style.cssText = "pointer-events: auto;";
+
+    clearInterval(modalTimerId);
+    btnModal[i].classList.add("active");
+  }
+
+  function btnClass() {
+      btnModal.forEach((item) => {
+        item.classList.remove("active");
+      });
+    }
+
+  hideModal();
+  // btnModal.addEventListener('click', e => {
+  //       modal.style.display = "block";
+  // });
+
+  btnWrapper.addEventListener("click", (e) => {
+    const target = e.target;
+    console.log("Hello");
+    if (target && target.classList.contains("btn-modal"))
+      btnModal.forEach((item, i) => {
+        if (target == item) {
+          // hideModal();
+          showModal(i);
+        }
+      });
+  });
+
+  // btnModal.forEach((item, i) => {
+  //       item.addEventListener('click', e => {
+  //             // btnModal[i].classList.add('active');
+  //             showModal(i);
+  //       });
+  // });
+
+  const button = document.createElement("button");
+
+  button.textContent = "Click";
+
+  button.classList.add("btn-modal");
+
+  btnWrapper.append(button);
+
+  button.addEventListener("click", (e) => {
+    showModal();
+  });
+
+
+  btnClose.addEventListener("click", (e) => {
+    modal.classList.toggle("hide");
+    document.body.style.overflow = "";
+    btnClass();
+  });
+
+  modal.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target === modal) {
       hideModal();
+    }
+    btnClass();
+  });
 
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      hideModal();
+    }
+    if (e.code === "KeyF") {
+      showModal();
+    }
+  });
 
-      // btnModal.addEventListener('click', e => {
-      //       modal.style.display = "block";
-      // });
+//   const modalTimerId = setTimeout(openModal, 3000);
 
-      // btnWrapper.addEventListener('click', e => {
-      //       const target = e.target;
-      //       console.log('Hello');
-      //       btnModal.forEach((item, i) => {
-      //             if (target == item) {
-      //       // modal.style.display = "block"
-      //       item.classList.add('active');
-      //       showModal();
-      //             }
-      //       });
+function showModalByScroll() {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+      }
+}
 
-      // });
-
-      btnModal.forEach(item => {
-            item.addEventListener('click', e => {
-                  showModal();
-
-            });
-      });
-
-      const button = document.createElement('button');
-
-      button.textContent = 'Click';
-
-      button.classList.add('btn-modal');
-
-      btnWrapper.append(button);
-
-      btnClose.addEventListener('click', e => {
-            modal.classList.toggle('hide');
-            document.body.style.overflow = '';
-      });
-      modal.addEventListener('click', e => {
-            const target = e.target;
-
-            if (target === modal) {
-                  hideModal();
-            }
-      });
-
-      document.addEventListener('keydown', (e) => {
-            if (e.code === "Escape") {
-                  hideModal();
-            }
-            if (e.code === "KeyF") {
-                  showModal();
-            }
-      });
-      
+  window.addEventListener('scroll', showModalByScroll);
+      // , {once: true}
 });
