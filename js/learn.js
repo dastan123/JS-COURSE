2796c06715e1c4852363568ac10045a8262cc75c
Dastan <dastan@gmail.com>
2021-03-08 07:41:05 +0600

    this

--- js/learn.js
+++ js/learn.js
@@ -1,26 +1,82 @@
 'use strict';
 
-function User(name, id) {
-      this.name = name;
-      this.id = id;
-      this.human = true;
-      this.hello = function() {
-            console.log(`Hello ${this.name}`);
-      };
-}
+// function showThis(a, b) {
+//       console.log(this);
+//       function sum() {
+//             console.log(this);
+//             return a + b;
+//       }
+//       console.log(sum());
+// }
 
-User.prototype.exit = function() {
-      console.log(`user ${this.name} exited`);
-};
+// showThis(4, 6);
 
-const Ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+// const obj = {
+//       a: 20,
+//       b: 15,
+//       sum: function() {
+//       console.log(this.a);
+//             function shout() { 
+//                   console.log(this);
+//             }
+//             shout();
+//       }
+// };
 
-Ivan.exit();
+// obj.sum();
 
-Ivan.hello();
-alex.hello();
+// function User(name, id) {
+//       this.name = name;
+//       this.id = id;
+//       this.human = true;
+// }
+// let ivan = new User('Ivan', 23);
 
-console.log(Ivan);
-console.log(alex);
+// function sayName() {
+//       console.log(this);
+//       console.log(this.name + this.surname );
+// }
 
+// const user = {
+//       name: 'Jonh',
+//       surname: 'Smith'
+// };
+
+// sayName.call(user);
+// sayName.apply(user);
+
+// function count(num) {
+//       return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+
+// 1) Обычная функция this = window, но если use strict = undefined
+// 2) Контекст у методов обЬекта это сам обьект
+// 3) this в конструкторах и классах - это новый экземпляр обьекта
+// 4) Ручная привязка this: call, apply, bind
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function(){ // only if function not working with =>
+//       this.style.backgroundColor = "blue";
+// });
+
+// const obj = {
+//       num: 5,
+//       sayNumber: function() {
+//             const say = () => { //this will work 
+//                   console.log(this.num);
+//             };
+//       say();
+//       }
+// };
+
+// obj.sayNumber();
+
+// const double = a => a * 2; // if only 1 stroke return automatic without ()
+
+// console.log(double(4));
